348c53a6f431536b8589f0ae8e5e34147e6a4494
viincyy <viincyy126@gmail.com>
2021-10-16 22:17:41 +0700

    Handle login api

--- Server/README.md
+++ Server/README.md
@@ -1,7 +1,35 @@
-## Run the server
+# Run the server
 
 ```
 npm run server
 ```
 
-## APIs
+# APIs
+
+## Register
+
+```
+POST http://localhost:8080/api/auth/register
+```
+
+## Login
+
+```
+POST http://localhost:8080/api/auth/login
+```
+
+# Models
+
+###### Users{
+
+    name: string
+    username: string
+    password: string
+    email: string
+    address: string
+    about: string
+    avatar: blob
+    cookpadId: string
+    userType: boolean
+
+}
--- Server/src/controllers/AuthController.js
+++ Server/src/controllers/AuthController.js
@@ -1,42 +1,59 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const database = require('../models/index');
+
+const salt = bcrypt.genSaltSync(10);
 
 class AuthController {
     // @route POST /api/auth/register
     // @desc Register user
     // @access public
     async register(req, res) {
-        // const { username, password } = req.body;
-        // // Simple validation
-        // if (!username || !password) {
-        //     return res.status(400).json({
-        //         success: false,
-        //         message: 'Missing username or password',
-        //     });
-        // }
-        // try {
-        //     // Check for existing user
-        //     if (user)
-        //         return res
-        //             .status(400)
-        //             .json({ success: false, message: 'User already exists' });
-        //     // All good
-        //     const hashedPassword = await argon2.hash(password);
-        //     // Return token
-        //     const accessToken = jwt.sign(
-        //         { userId: newUser._id },
-        //         process.env.ACCESS_TOKEN_SECRET
-        //     );
-        //     res.json({
-        //         success: true,
-        //         message: 'User created successfully',
-        //         accessToken,
-        //     });
-        // } catch (error) {
-        //     res.status(500).json({
-        //         success: 'false',
-        //         message: 'Internal server error',
-        //     });
-        // }
+        const { name, username, password, email, userType } = req.body;
+        // Simple validation
+        if (!username || !password) {
+            return res.status(400).json({
+                success: false,
+                message: 'Missing username or password',
+            });
+        }
+        try {
+            const user = await database.User.findOne({ where: { username } });
+            // Check for existing user
+            if (user)
+                return res
+                    .status(400)
+                    .json({ success: false, message: 'User already exists' });
+
+            // All good
+            const hashedPassword = bcrypt.hashSync(password, salt);
+            const newUser = {
+                name,
+                username,
+                password: hashedPassword,
+                email,
+                userType,
+            };
+
+            await database.User.create(newUser);
+
+            // Return token
+            const accessToken = jwt.sign(
+                { userId: newUser.id },
+                process.env.ACCESS_TOKEN_SECRET
+            );
+
+            res.json({
+                success: true,
+                message: 'User has been created successfully',
+                accessToken,
+            });
+        } catch (error) {
+            res.status(500).json({
+                success: 'false',
+                message: 'Internal server error',
+            });
+        }
     }
     // @route POST /api/auth/login
     // @desc Login user
