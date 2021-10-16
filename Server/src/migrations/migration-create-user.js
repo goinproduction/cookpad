cdc3f3bce811db1012cb99b0613e6fbba500aa05
viincyy <viincyy126@gmail.com>
2021-10-16 18:42:26 +0700

    Handle user migration, model

--- Server/src/migrations/migration-create-user.js
+++ Server/src/migrations/migration-create-user.js
@@ -1,33 +1,51 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      firstName: {
-        type: Sequelize.STRING
-      },
-      lastName: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING(50),
+            },
+            username: {
+                type: Sequelize.STRING(15),
+            },
+            password: {
+                type: Sequelize.STRING(50),
+            },
+            email: {
+                type: Sequelize.STRING(50),
+            },
+            address: {
+                type: Sequelize.STRING(255),
+            },
+            about: {
+                type: Sequelize.STRING(255),
+            },
+            avatar: {
+                type: Sequelize.BLOB('long'),
+            },
+            cookpadId: {
+                type: Sequelize.STRING(50),
+            },
+            userType: {
+                type: Sequelize.BOOLEAN,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Users');
+    },
 };
--- Server/src/models/user.js
+++ Server/src/models/user.js
@@ -1,25 +1,32 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
     }
-  };
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+    User.init(
+        {
+            name: DataTypes.STRING(50),
+            username: DataTypes.STRING(15),
+            password: DataTypes.STRING(50),
+            email: DataTypes.STRING(50),
+            address: DataTypes.STRING(255),
+            about: DataTypes.STRING(255),
+            avatar: DataTypes.BLOB('long'),
+            cookpadId: DataTypes.STRING(50),
+            userType: DataTypes.BOOLEAN,
+        },
+        {
+            sequelize,
+            modelName: 'User',
+        }
+    );
+    return User;
 };
