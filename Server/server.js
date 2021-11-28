8165647082fe7c9f02f4cf7f12f148382a5a3f02
ryann126 <viincyy126@gmail.com>
2021-11-29 00:23:27 +0700

    Fix overisze image

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,12 @@ ConnectToDatabase();
 
 const app = express();
 app.use("/assets/img", express.static("assets/img"));
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
+app.use(express.urlencoded({
+    limit: '50mb'
+}));
 app.use(cors());
 
 route(app);
